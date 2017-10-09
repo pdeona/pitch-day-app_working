fb49a7ed62dd367f249beb5564c602dce2df3769
Dwayne Richard Lewis <dwayne@fashiongatekeepers.com>
2017-10-09 12:28:08 -0400

    uploading landing page views

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,8 +1,8 @@
-$(document).on('turbolinks:load', function() {
-
-  console.log('beer?');
-  window.Trello.authorize();
-  let response = window.Trello.get('members/me', (response) => {$.post('http://localhost:3000/user', response)});
-
-
-});
+// $(document).on('turbolinks:load', function() {
+//
+//   console.log('beer?');
+//   window.Trello.authorize();
+//   let response = window.Trello.get('members/me', (response) => {$.post('http://localhost:3000/user', response)});
+//
+//
+// });
--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,5 @@
 class LandingController < ApplicationController
   def index
-    redirect_to login_path if current_user.nil?
+    # redirect_to login_path if current_user.nil?
   end
 end
